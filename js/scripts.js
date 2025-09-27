fb81824d1912947d767d03bb910445010a74c045
Caio Moura <caio_mcamargo@hotmail.com>
2025-09-27 19:14:10 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,13 +16,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// Adicione este bloco no seu scripts.js
 
-// Pega o elemento do título pelo ID que acabamos de criar
+
+
 const titulo = document.getElementById('titulo-principal');
 
-// Define um timer para parar a animação do cursor
+
 setTimeout(() => {
-    // Adiciona a classe 'typing-done' ao h1 depois de 3.5 segundos
+   
     titulo.classList.add('typing-done');
-}, 3500); // O tempo aqui é em milissegundos (3500ms = 3.5s)
+}, 3500); 
